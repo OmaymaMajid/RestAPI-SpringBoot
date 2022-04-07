2556e0d8c0c92df06652c96a7d85c50058ecc7da
MAJID Omayma <o.majid@abalsia.com>
2022-04-07 11:55:47 +0000

    API Dev

---
+++ src/main/java/com/hr/api/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.hr.api.repository;
+
+import com.hr.api.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+}
